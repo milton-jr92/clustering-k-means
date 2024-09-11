d62921815cb2ad20d635f108d25fbfd1ab6b8132
milton-jr92 <milton.jr92@gmail.com>
2024-09-11 11:34:10 -0300

    feat: finish kmeans auxiliar methods and create a test case

--- kmeans.py
+++ kmeans.py
@@ -26,52 +26,86 @@ class KMeans(Generic[Point]):
         centroid: DataPoint
 
 
-def __init__(self, k: int, points: List[Point]) -> None:
-    if k < 1:  # k-means does not work with negative clusters or equals to zero
-        raise ValueError("k must be >= 1")
-    self._points: List[Point] = points
-    self._zscore_normalize()
-    # initialize empty clusters with random centroids
-    self._clusters: List[KMeans.Cluster] = []
-    for _ in range(k):
-        rand_point: DataPoint = self._random_point()
-        cluster: KMeans.Cluster = KMeans.Cluster([], rand_point)
-        self._clusters.append(cluster)
+    def __init__(self, k: int, points: List[Point]) -> None:
+        if k < 1:  # k-means does not work with negative clusters or equals to zero
+            raise ValueError("k must be >= 1")
+        self._points: List[Point] = points
+        self._zscore_normalize()
+        # initialize empty clusters with random centroids
+        self._clusters: List[KMeans.Cluster] = []
+        for _ in range(k):
+            rand_point: DataPoint = self._random_point()
+            cluster: KMeans.Cluster = KMeans.Cluster([], rand_point)
+            self._clusters.append(cluster)
+    
+            
+    def run(self, max_iterations: int = 100) -> List[KMeans.Cluster]:
+        for it in range(max_iterations):
+            for cluster in self._clusters:
+                cluster.points.clear()
+            self._assign_clusters()
+            old_centroids: List[DataPoint] = deepcopy(self._centroids)
+            self._generate_centroids()
+            if old_centroids == self._centroids:
+                print(f"Converged after {it} iterations")
+                return self._clusters
+        return self._clusters
 
 
-@property
-def _centroids(self) -> List[DataPoint]:
-    return [x.centroid for x in self._clusters]
+    @property
+    def _centroids(self) -> List[DataPoint]:
+        return [x.centroid for x in self._clusters]
 
 
-def _dimension_slice(self, dimension: int) -> List[float]:
-    return [x.dimensions[dimension] for x in self._points]
+    def _dimension_slice(self, dimension: int) -> List[float]:
+        return [x.dimensions[dimension] for x in self._points]
 
 
-def _zcore_normalize(self) -> None:
-    zscored: List[List[float]] = [[] for _ in range(len(self._points))]
-    for dimension in range(self._points[0].num_dimensions):
-        dimension_slice: List[float] = self._dimension_slice(dimension)
-        for index, zscore in enumerate(zscore(dimension_slice)):
-            zscored[index].append(zscore)
-    for i in range(len(self._points)):
-        self._points[i].dimensions = tuple(zscored[i])
+    def _zscore_normalize(self) -> None:
+        zscored: List[List[float]] = [[] for _ in range(len(self._points))]
+        for dimension in range(self._points[0].num_dimensions):
+            dimension_slice: List[float] = self._dimension_slice(dimension)
+            for index, zscore in enumerate(zscores(dimension_slice)):
+                zscored[index].append(zscore)
+        for i in range(len(self._points)):
+            self._points[i].dimensions = tuple(zscored[i])
 
 
-def _random_point(self) -> DataPoint:
-    rand_dimensions: List[float] = []
-    for dimension in range(self._points[0].num_dimensions):
-        values: List[float] = self._dimension_slice(dimension)
-        rand_value: float = uniform(min(value), max(values))
-        rand_dimensions.append(rand_value)
-    return DataPoint(rand_dimensions)
+    def _random_point(self) -> DataPoint:
+        rand_dimensions: List[float] = []
+        for dimension in range(self._points[0].num_dimensions):
+            values: List[float] = self._dimension_slice(dimension)
+            rand_value: float = uniform(min(values), max(values))
+            rand_dimensions.append(rand_value)
+        return DataPoint(rand_dimensions)
 
 
-# finds the closest cluster centroid to each point
-# and assigns the point to that cluster
-def _assign_clusters(self) -> None:
-    for point in self._points:
-        closest: DataPoint = min(self._centroids, key=partial(DataPoint.distance, point))
-        idx: int = self._centroids.index(closest)
-        cluster: KMeans.Cluster = self._clusters[idx]
-        cluster.points.append(point)
+    # finds the closest cluster centroid to each point
+    # and assigns the point to that cluster
+    def _assign_clusters(self) -> None:
+        for point in self._points:
+            closest: DataPoint = min(self._centroids, key=partial(DataPoint.distance, point))
+            idx: int = self._centroids.index(closest)
+            cluster: KMeans.Cluster = self._clusters[idx]
+            cluster.points.append(point)
+
+    # finds center of each cluster and deslocate the centroid to that point
+    def _generate_centroids(self) -> None:
+        for cluster in self._clusters:
+            if len(cluster.points) == 0:
+                continue
+            means: List[float] = []
+            for dimension in range(cluster.points[0].num_dimensions):
+                dimension_slice: List[float] = [p.dimensions[dimension] for p in cluster.points]
+                means.append(mean(dimension_slice))
+            cluster.centroid = DataPoint(means)
+
+
+if __name__ == "__main__":
+    point1: DataPoint = DataPoint([2.0, 1.0, 1.0])
+    point2: DataPoint = DataPoint([2.0, 2.0, 5.0])
+    point3: DataPoint = DataPoint([3.0, 1.5, 2.5])
+    kmeans_test: KMeans[DataPoint] = KMeans(2, [point1, point2, point3])
+    test_clusters: List[KMeans.Cluster] = kmeans_test.run()
+    for index, cluster in enumerate(test_clusters):
+        print(f"Cluster {index}: {cluster.points}")
